18e80d13b82a27fb9d1d0dc327379d082cbd00c6
umut oğur <baxan_umut@hotmail.com>
2019-09-29 01:07:12 +0300

    first commit

---
+++ VeriTabanınaResimEkleme/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace VeriTabanınaResimEkleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txttur = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtpuan = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtkategori = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtresim = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dbo_FilmArsivDataSet = new VeriTabanınaResimEkleme.Dbo_FilmArsivDataSet();
+            this.tblFilmlerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_FilmlerTableAdapter = new VeriTabanınaResimEkleme.Dbo_FilmArsivDataSetTableAdapters.Tbl_FilmlerTableAdapter();
+            this.filmIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filmAdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filmTurDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filmPaunDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filmKategoriDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.filmResimDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbo_FilmArsivDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblFilmlerBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Film ID:";
+            // 
+            // txtid
+            // 
+            this.txtid.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtid.Location = new System.Drawing.Point(99, 12);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 24);
+            this.txtid.TabIndex = 1;
+            // 
+            // txtad
+            // 
+            this.txtad.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtad.Location = new System.Drawing.Point(99, 60);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 24);
+            this.txtad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Film ADI:";
+            // 
+            // txttur
+            // 
+            this.txttur.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttur.Location = new System.Drawing.Point(99, 110);
+            this.txttur.Name = "txttur";
+            this.txttur.Size = new System.Drawing.Size(100, 24);
+            this.txttur.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TÜRÜ:";
+            // 
+            // txtpuan
+            // 
+            this.txtpuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpuan.Location = new System.Drawing.Point(99, 155);
+            this.txtpuan.Name = "txtpuan";
+            this.txtpuan.Size = new System.Drawing.Size(100, 24);
+            this.txtpuan.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PUAN:";
+            // 
+            // txtkategori
+            // 
+            this.txtkategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkategori.Location = new System.Drawing.Point(99, 194);
+            this.txtkategori.Name = "txtkategori";
+            this.txtkategori.Size = new System.Drawing.Size(100, 24);
+            this.txtkategori.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "KATAGORİ:";
+            // 
+            // txtresim
+            // 
+            this.txtresim.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtresim.Location = new System.Drawing.Point(99, 233);
+            this.txtresim.Name = "txtresim";
+            this.txtresim.Size = new System.Drawing.Size(100, 24);
+            this.txtresim.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(38, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 18);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "RESİM";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "KAYDET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(99, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "GÜNCELLE";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(205, 234);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(33, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(250, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(417, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.filmIdDataGridViewTextBoxColumn,
+            this.filmAdDataGridViewTextBoxColumn,
+            this.filmTurDataGridViewTextBoxColumn,
+            this.filmPaunDataGridViewTextBoxColumn,
+            this.filmKategoriDataGridViewCheckBoxColumn,
+            this.filmResimDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tblFilmlerBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 333);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(644, 134);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dbo_FilmArsivDataSet
+            // 
+            this.dbo_FilmArsivDataSet.DataSetName = "Dbo_FilmArsivDataSet";
+            this.dbo_FilmArsivDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblFilmlerBindingSource
+            // 
+            this.tblFilmlerBindingSource.DataMember = "Tbl_Filmler";
+            this.tblFilmlerBindingSource.DataSource = this.dbo_FilmArsivDataSet;
+            // 
+            // tbl_FilmlerTableAdapter
+            // 
+            this.tbl_FilmlerTableAdapter.ClearBeforeFill = true;
+            // 
+            // filmIdDataGridViewTextBoxColumn
+            // 
+            this.filmIdDataGridViewTextBoxColumn.DataPropertyName = "FilmId";
+            this.filmIdDataGridViewTextBoxColumn.HeaderText = "FilmId";
+            this.filmIdDataGridViewTextBoxColumn.Name = "filmIdDataGridViewTextBoxColumn";
+            this.filmIdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // filmAdDataGridViewTextBoxColumn
+            // 
+            this.filmAdDataGridViewTextBoxColumn.DataPropertyName = "FilmAd";
+            this.filmAdDataGridViewTextBoxColumn.HeaderText = "FilmAd";
+            this.filmAdDataGridViewTextBoxColumn.Name = "filmAdDataGridViewTextBoxColumn";
+            // 
+            // filmTurDataGridViewTextBoxColumn
+            // 
+            this.filmTurDataGridViewTextBoxColumn.DataPropertyName = "FilmTur";
+            this.filmTurDataGridViewTextBoxColumn.HeaderText = "FilmTur";
+            this.filmTurDataGridViewTextBoxColumn.Name = "filmTurDataGridViewTextBoxColumn";
+            // 
+            // filmPaunDataGridViewTextBoxColumn
+            // 
+            this.filmPaunDataGridViewTextBoxColumn.DataPropertyName = "FilmPaun";
+            this.filmPaunDataGridViewTextBoxColumn.HeaderText = "FilmPaun";
+            this.filmPaunDataGridViewTextBoxColumn.Name = "filmPaunDataGridViewTextBoxColumn";
+            // 
+            // filmKategoriDataGridViewCheckBoxColumn
+            // 
+            this.filmKategoriDataGridViewCheckBoxColumn.DataPropertyName = "FilmKategori";
+            this.filmKategoriDataGridViewCheckBoxColumn.HeaderText = "FilmKategori";
+            this.filmKategoriDataGridViewCheckBoxColumn.Name = "filmKategoriDataGridViewCheckBoxColumn";
+            // 
+            // filmResimDataGridViewTextBoxColumn
+            // 
+            this.filmResimDataGridViewTextBoxColumn.DataPropertyName = "FilmResim";
+            this.filmResimDataGridViewTextBoxColumn.HeaderText = "FilmResim";
+            this.filmResimDataGridViewTextBoxColumn.Name = "filmResimDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 465);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtresim);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtkategori);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtpuan);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txttur);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbo_FilmArsivDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblFilmlerBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txttur;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtpuan;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtkategori;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtresim;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private Dbo_FilmArsivDataSet dbo_FilmArsivDataSet;
+        private System.Windows.Forms.BindingSource tblFilmlerBindingSource;
+        private Dbo_FilmArsivDataSetTableAdapters.Tbl_FilmlerTableAdapter tbl_FilmlerTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filmIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filmAdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filmTurDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filmPaunDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn filmKategoriDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filmResimDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ VeriTabanınaResimEkleme/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace VeriTabanınaResimEkleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-T2GKN5C\SQLEXPRESS;Initial Catalog=Dbo_FilmArsiv;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dbo_FilmArsivDataSet.Tbl_Filmler' table. You can move, or remove it, as needed.
+            this.tbl_FilmlerTableAdapter.Fill(this.dbo_FilmArsivDataSet.Tbl_Filmler);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+            txtresim.Text = openFileDialog1.FileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into Tbl_Filmler(FilmAd,FilmTur,FilmPaun,FilmKategori,FilmResim) values (@p1,@p2,@p3,@p4,@p5)", baglanti);
+            komut.Parameters.AddWithValue("@p1", txtad.Text);
+            komut.Parameters.AddWithValue("@p2", txttur.Text);
+            komut.Parameters.AddWithValue("@p3",float.Parse( txtpuan.Text));
+            komut.Parameters.AddWithValue("@p4", txtkategori.Text);
+            komut.Parameters.AddWithValue("@p5", txtresim.Text);
+            komut.ExecuteNonQuery();
+            MessageBox.Show("İşlem Tamamlandı.");
+            baglanti.Close();
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            txtid.Text = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            txtad.Text = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            txttur.Text = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            txtpuan.Text = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            txtkategori.Text = dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+            txtresim.Text = dataGridView1.Rows[secilen].Cells[5].Value.ToString();
+            pictureBox1.ImageLocation= dataGridView1.Rows[secilen].Cells[5].Value.ToString();
+        }
+    }
+}
